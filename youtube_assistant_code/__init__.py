af04d3982621943eedbac89fbb99435b0b84f674
unknown <lehuytran2222@gmail.com>
2024-05-17 14:48:55 +0700

    Remove mussic assistant

--- youtube_assistant_code/__init__.py
+++ youtube_assistant_code/__init__.py
@@ -1,69 +1,125 @@
 # encoding: utf-8
-import json
-import random
-from time import sleep
-from urllib.parse import parse_qs, urlparse
-
-import requests
-import voluptuous as vol
 from email.policy import default
+import json
+from numpy import number
+import requests
+import random
+from urllib.parse import parse_qs, urlparse
 from homeassistant.const import ATTR_ENTITY_ID
 from homeassistant.helpers import config_validation as cv, service
-from pytube import YouTube
-
+import voluptuous as vol
 from .youtube_assistant import *
 import logging
-
+from pytube import YouTube
 _LOGGER = logging.getLogger(__name__)
-
+# Declare variables
 DOMAIN = 'youtube_assistant'
 
 SERVICE_PLAY_SONG = 'play_song'
 SERVICE_PLAY_LIST = 'play_list'
-SERVICE_PLAY_NEXT = 'play_next'
-SERVICE_PLAY_PREVIOUS = 'play_previous'
-
+SERVICE_PLAY_NEXT='play_next'
+SERVICE_PLAY_PREVIOUS='play_previous'
+# config
+# CONF_API_KEY = 'api_key'
+# data service
+# ATTR_PLAYER_ID = 'entity_id'
 ATTR_NAME = 'name'
 ATTR_SONG_ID = 'song_id'
 ATTR_URL = 'url'
 ATTR_NUMBER = 'number'
 ATTR_LIST_ID = 'list_id'
 ATTR_REPEAT = 'repeat'
+ID_SONG = ''
+OLD_ID_PLAYLIST=''
+OLD_PLAYLIST = []
 
 SERVICE_SONG = vol.Schema({
-    vol.Required(ATTR_ENTITY_ID): cv.comp_entity_ids,
-    vol.Optional(ATTR_SONG_ID, default=""): cv.string,
-    vol.Optional(ATTR_URL, default=""): cv.string,
-    vol.Optional(ATTR_NAME, default=""): cv.string,
-    vol.Optional(ATTR_REPEAT, default=False): cv.boolean,
-    vol.Optional(ATTR_NUMBER, default=0): cv.positive_int
-})
-
+        vol.Required(ATTR_ENTITY_ID): cv.comp_entity_ids,
+        vol.Optional(ATTR_SONG_ID, default=""): cv.string,
+        vol.Optional(ATTR_URL, default=""): cv.string,
+        vol.Optional(ATTR_NAME, default=""): cv.string,
+        vol.Optional(ATTR_REPEAT, default=False): cv.boolean,
+        vol.Optional(ATTR_NUMBER, default=0): cv.positive_int
+    }
+)
 SERVICE_LIST = vol.Schema({
-    vol.Required(ATTR_ENTITY_ID): cv.comp_entity_ids,
-    vol.Optional(ATTR_LIST_ID, default=""): cv.string,
-    vol.Optional(ATTR_URL, default=""): cv.string
-})
-
+        vol.Required(ATTR_ENTITY_ID): cv.comp_entity_ids,
+        vol.Optional(ATTR_LIST_ID, default=""): cv.string,
+        vol.Optional(ATTR_URL, default=""): cv.string   
+    }
+)
 SERVICE_NEXT = vol.Schema({
-    vol.Required(ATTR_ENTITY_ID): cv.comp_entity_ids,
-    vol.Optional(ATTR_LIST_ID, default=""): cv.string,
-    vol.Optional(ATTR_URL, default=""): cv.string,
-    vol.Optional(ATTR_SONG_ID, default=""): cv.string,
-    vol.Optional(ATTR_NAME, default=""): cv.string,
-    vol.Optional(ATTR_REPEAT, default=False): cv.boolean,
-    vol.Optional(ATTR_NUMBER, default=0): cv.positive_int
-})
-
+        vol.Required(ATTR_ENTITY_ID): cv.comp_entity_ids,
+        vol.Optional(ATTR_LIST_ID, default=""): cv.string,
+        vol.Optional(ATTR_URL, default=""): cv.string,
+        vol.Optional(ATTR_SONG_ID, default=""): cv.string,
+        vol.Optional(ATTR_NAME, default=""): cv.string,
+        vol.Optional(ATTR_REPEAT, default=False): cv.boolean,
+        vol.Optional(ATTR_NUMBER, default=0): cv.positive_int
+    }
+)
 SERVICE_PREVIOUS = vol.Schema({
-    vol.Required(ATTR_ENTITY_ID): cv.comp_entity_ids,
-    vol.Optional(ATTR_LIST_ID, default=""): cv.string,
-    vol.Optional(ATTR_URL, default=""): cv.string,
-    vol.Optional(ATTR_SONG_ID, default=""): cv.string,
-    vol.Optional(ATTR_NAME, default=""): cv.string,
-    vol.Optional(ATTR_REPEAT, default=False): cv.boolean,
-    vol.Optional(ATTR_NUMBER, default=0): cv.positive_int
-})
+        vol.Required(ATTR_ENTITY_ID): cv.comp_entity_ids,
+        vol.Optional(ATTR_LIST_ID, default=""): cv.string,
+        vol.Optional(ATTR_URL, default=""): cv.string,
+        vol.Optional(ATTR_SONG_ID, default=""): cv.string,
+        vol.Optional(ATTR_NAME, default=""): cv.string,
+        vol.Optional(ATTR_REPEAT, default=False): cv.boolean,
+        vol.Optional(ATTR_NUMBER, default=0): cv.positive_int
+    }
+)
+
+from time import sleep
+global stop
+stop =False
+def play_playlist(hass, entity_id, list_playlist,stop):
+    for song_id in list_playlist:
+        # Phát bài hát
+        if stop == True:
+            break
+        else:
+            _LOGGER.info("Đang phát bài song_id: %s", song_id)
+            global ID_SONG
+            ID_SONG = song_id
+            yt = YouTube(return_url_from_id(song_id))
+            service_data = {'entity_id': entity_id, 'media_content_id': return_url_from_id(song_id), 'media_content_type': 'music',"extra": {"thumb": yt.thumbnail_url,"title": yt.title}}
+            hass.services.call('media_player', 'play_media', service_data)
+            #Đợi một khoảng thời gian ngắn trước check state
+            sleep(3)
+            while True:
+                # Lấy state của media player
+                state = hass.states.get(entity_id)
+                # _LOGGER.info("state: %s", state.state)
+                # Nếu state là idle thì bài hát đã kết thúc ->break
+                if state.state == 'idle':
+                    break
+                sleep(1)
+
+def play_next(hass, entity_id,list_playlist):
+    # Lấy index hiện tại
+    current_index = list_playlist.index(ID_SONG)
+    # Nếu index hiện tại nhỏ hơn độ dài playlist - 1 thì tăng index lên 1
+    if current_index < len(list_playlist) - 1:
+        current_index += 1
+    # Phát bài hát tiếp theo
+    stop = True
+    sleep(1)
+    stop = False
+    play_playlist(hass, entity_id, list_playlist[current_index:],stop)
+
+def play_previous(hass, entity_id, list_playlist):
+    # Lấy index hiện tại
+    current_index = list_playlist.index(ID_SONG)
+    # Nếu index hiện tại lớn hơn 0 thì giảm index đi 1
+    if current_index > 0:
+        current_index -= 1
+    # Phát bài hát trước đó
+    stop= True
+    sleep(1)
+    stop = False
+    _LOGGER.info("list song_id: %s",list_playlist[current_index:])
+    play_playlist(hass, entity_id, list_playlist[current_index:],stop)
+
 def get_playlist_from_id_url(list_id, url):
     id=''
     if (list_id != ""):
@@ -83,113 +139,76 @@ def get_playlist_from_songid_name(song_id,url,name):
         id = return_id_from_name(name)
     song_url = return_url_from_id(id)  
     return song_url,id 
-    
-class PlayerMedia:
-    def __init__(self, hass):
-        self.hass = hass
-        self.old_id_playlist = ''
-        self.old_playlist = []
-        self.id_song = ''
-        self.stop = False
 
-    def play_playlist(self, entity_id, list_playlist):
-        _LOGGER.info("list: %s", list_playlist)
-        for song_id in list_playlist:
-            if self.stop:
-                break
-            _LOGGER.info("Playing song with ID: %s", song_id)
-            self.id_song = song_id
-            yt = YouTube(return_url_from_id(song_id))
-            service_data = {
-                'entity_id': entity_id,
-                'media_content_id': return_url_from_id(song_id),
-                'media_content_type': 'music',
-                "extra": {
-                    "thumb": yt.thumbnail_url,
-                    "title": yt.title
-                }
-            }
-            self.hass.services.call('media_player', 'play_media', service_data)
-            sleep(3)
-            while True:
-                state = self.hass.states.get(entity_id)
-                if state.state == 'idle':
-                    break
-                sleep(1)
+def setup(hass, config):
 
-    def play_next(self, entity_id, list_playlist):
-        current_index = list_playlist.index(self.id_song)
-        if current_index < len(list_playlist) - 1:
-            current_index += 1
-        self.stop = True
-        self.stop = False
-        self.play_playlist(entity_id, list_playlist[current_index:])
+    def tts_handler(service):
 
-    def play_previous(self, entity_id, list_playlist):
-        current_index = list_playlist.index(self.id_song)
-        if current_index > 0:
-            current_index -= 1
-        self.stop = True
-        self.stop = False
-        self.play_playlist(entity_id, list_playlist[current_index:])
-
-    def tts_handler(self, service):
         entity_id = service.data[ATTR_ENTITY_ID]
+        service_data = {}
+        service_play = {'entity_id': entity_id, 'command': 'play'}
+        global OLD_ID_PLAYLIST
+        global OLD_PLAYLIST
         for entity in entity_id:
             if service.service == SERVICE_PLAY_LIST:
                 list_id = service.data.get(ATTR_LIST_ID)
+                _LOGGER.info("-"*20)
                 url = service.data.get(ATTR_URL)
                 list_playlist = get_playlist_from_id_url(list_id, url)
-                self.old_id_playlist = list_id
-                self.old_playlist = list_playlist
-                self.play_playlist(entity, list_playlist)
+                _LOGGER.info("list song_id: %s", list_playlist)
+                _LOGGER.info("-"*20)
+                OLD_ID_PLAYLIST = list_id
+                OLD_PLAYLIST = list_playlist
+                play_playlist(hass, entity, list_playlist,stop)
             elif service.service == SERVICE_PLAY_SONG:
                 song_id = service.data.get(ATTR_SONG_ID)
                 url = service.data.get(ATTR_URL)
                 number = service.data.get(ATTR_NUMBER)
                 name = service.data.get(ATTR_NAME)
                 repeat = service.data.get(ATTR_REPEAT)
-                song_url, id = get_playlist_from_songid_name(song_id, url, name)
-                self.old_id_playlist = id
-                if repeat:
+                song_url,id = get_playlist_from_songid_name(song_id,url,name)
+                OLD_ID_PLAYLIST = id
+                if (repeat == True):
                     if number != 0:
-                        list_playlist = [song_url] * number
-                        self.old_playlist = list_playlist
-                        self.play_playlist(entity, list_playlist)
+                        list_playlist = [song_url]*number
+                        OLD_PLAYLIST = list_playlist
+                        play_playlist(hass, entity, list_playlist,stop)
                 else:
                     if number != 0:
-                        list_id_the_same = self.return_the_same_id(id, number)
-                        self.old_playlist = list_id_the_same
-                        self.play_playlist(entity, list_id_the_same)
+                        list_id_the_same = return_the_same_id(id, number)
+                        OLD_PLAYLIST = list_id_the_same
+                        play_playlist(hass, entity, list_id_the_same,stop)
             elif service.service == SERVICE_PLAY_NEXT:
-                self.handle_next_previous(service, entity, True)
+                list_id = service.data.get(ATTR_LIST_ID)
+                song_id = service.data.get(ATTR_SONG_ID)
+                url = service.data.get(ATTR_URL)
+                number = service.data.get(ATTR_NUMBER)
+                name = service.data.get(ATTR_NAME)
+                repeat = service.data.get(ATTR_REPEAT)
+                id=''
+                if name:
+                    song_url,id = get_playlist_from_songid_name(song_id,url,name)
+                if (list_id == OLD_ID_PLAYLIST or id == OLD_ID_PLAYLIST):
+                    list_playlist = OLD_PLAYLIST
+                    play_next(hass, entity, list_playlist)
             elif service.service == SERVICE_PLAY_PREVIOUS:
-                self.handle_next_previous(service, entity, False)
+                list_id = service.data.get(ATTR_LIST_ID)
+                song_id = service.data.get(ATTR_SONG_ID)
+                url = service.data.get(ATTR_URL)
+                number = service.data.get(ATTR_NUMBER)
+                name = service.data.get(ATTR_NAME)
+                repeat = service.data.get(ATTR_REPEAT)
+                id=''
+                if name:
+                    song_url,id = get_playlist_from_songid_name(song_id,url,name)
+                if (list_id == OLD_ID_PLAYLIST or id == OLD_ID_PLAYLIST):
+                    list_playlist = OLD_PLAYLIST
+                    play_previous(hass, entity, list_playlist)
 
-    def handle_next_previous(self, service, entity, is_next):
-        list_id = service.data.get(ATTR_LIST_ID)
-        song_id = service.data.get(ATTR_SONG_ID)
-        url = service.data.get(ATTR_URL)
-        number = service.data.get(ATTR_NUMBER)
-        name = service.data.get(ATTR_NAME)
-        repeat = service.data.get(ATTR_REPEAT)
-        id = ''
-        if name:
-            song_url, id = get_playlist_from_songid_name(song_id, url, name)
-        if list_id == self.old_id_playlist or id == self.old_id_playlist:
-            list_playlist = self.old_playlist
-            if is_next:
-                self.play_next(entity, list_playlist)
-            else:
-                self.play_previous(entity, list_playlist)
 
-    def register_services(self):
-        self.hass.services.register(DOMAIN, SERVICE_PLAY_SONG, self.tts_handler, schema=SERVICE_SONG)
-        self.hass.services.register(DOMAIN, SERVICE_PLAY_LIST, self.tts_handler, schema=SERVICE_LIST)
-        self.hass.services.register(DOMAIN, SERVICE_PLAY_NEXT, self.tts_handler, schema=SERVICE_NEXT)
-        self.hass.services.register(DOMAIN, SERVICE_PLAY_PREVIOUS, self.tts_handler, schema=SERVICE_PREVIOUS)    
+    hass.services.register(DOMAIN, SERVICE_PLAY_SONG, tts_handler, schema=SERVICE_SONG)
+    hass.services.register(DOMAIN, SERVICE_PLAY_LIST, tts_handler, schema=SERVICE_LIST)
+    hass.services.register(DOMAIN, SERVICE_PLAY_NEXT, tts_handler, schema=SERVICE_NEXT)
+    hass.services.register(DOMAIN, SERVICE_PLAY_PREVIOUS, tts_handler, schema=SERVICE_PREVIOUS)
 
-def setup(hass, config):
-    youtube_assistant = PlayerMedia(hass)
-    youtube_assistant.register_services()
     return True
