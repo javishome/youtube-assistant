5ca0ccad753be225e4a8ac55becd15fb9cae283f
unknown <lehuytran2222@gmail.com>
2024-05-17 13:52:14 +0700

    Remove mussic assistant

--- youtube_assistant_code/__init__.py
+++ youtube_assistant_code/__init__.py
@@ -1,25 +1,28 @@
 # encoding: utf-8
-from email.policy import default
 import json
-from numpy import number
-import requests
 import random
+from time import sleep
 from urllib.parse import parse_qs, urlparse
+
+import requests
+import voluptuous as vol
+from email.policy import default
 from homeassistant.const import ATTR_ENTITY_ID
 from homeassistant.helpers import config_validation as cv, service
-import voluptuous as vol
+from pytube import YouTube
+
 from .youtube_assistant import *
 import logging
+
 _LOGGER = logging.getLogger(__name__)
-# Declare variables
+
 DOMAIN = 'youtube_assistant'
 
 SERVICE_PLAY_SONG = 'play_song'
 SERVICE_PLAY_LIST = 'play_list'
-# config
-# CONF_API_KEY = 'api_key'
-# data service
-# ATTR_PLAYER_ID = 'entity_id'
+SERVICE_PLAY_NEXT = 'play_next'
+SERVICE_PLAY_PREVIOUS = 'play_previous'
+
 ATTR_NAME = 'name'
 ATTR_SONG_ID = 'song_id'
 ATTR_URL = 'url'
@@ -28,109 +31,165 @@ ATTR_LIST_ID = 'list_id'
 ATTR_REPEAT = 'repeat'
 
 SERVICE_SONG = vol.Schema({
-        vol.Required(ATTR_ENTITY_ID): cv.comp_entity_ids,
-        vol.Optional(ATTR_SONG_ID, default=""): cv.string,
-        vol.Optional(ATTR_URL, default=""): cv.string,
-        vol.Optional(ATTR_NAME, default=""): cv.string,
-        vol.Optional(ATTR_REPEAT, default=False): cv.boolean,
-        vol.Optional(ATTR_NUMBER, default=0): cv.positive_int
-    }
-)
-SERVICE_LIST = vol.Schema({
-        vol.Required(ATTR_ENTITY_ID): cv.comp_entity_ids,
-        vol.Optional(ATTR_LIST_ID, default=""): cv.string,
-        vol.Optional(ATTR_URL, default=""): cv.string   
-    }
-)
-def setup(hass, config):
-    def clear_queue(entity_id):
-        service_data = {'command': 'clear', 'entity_id': entity_id}
-        hass.services.call('mass', 'queue_command', service_data)
-    def tts_handler(service):
+    vol.Required(ATTR_ENTITY_ID): cv.comp_entity_ids,
+    vol.Optional(ATTR_SONG_ID, default=""): cv.string,
+    vol.Optional(ATTR_URL, default=""): cv.string,
+    vol.Optional(ATTR_NAME, default=""): cv.string,
+    vol.Optional(ATTR_REPEAT, default=False): cv.boolean,
+    vol.Optional(ATTR_NUMBER, default=0): cv.positive_int
+})
 
+SERVICE_LIST = vol.Schema({
+    vol.Required(ATTR_ENTITY_ID): cv.comp_entity_ids,
+    vol.Optional(ATTR_LIST_ID, default=""): cv.string,
+    vol.Optional(ATTR_URL, default=""): cv.string
+})
+
+SERVICE_NEXT = vol.Schema({
+    vol.Required(ATTR_ENTITY_ID): cv.comp_entity_ids,
+    vol.Optional(ATTR_LIST_ID, default=""): cv.string,
+    vol.Optional(ATTR_URL, default=""): cv.string,
+    vol.Optional(ATTR_SONG_ID, default=""): cv.string,
+    vol.Optional(ATTR_NAME, default=""): cv.string,
+    vol.Optional(ATTR_REPEAT, default=False): cv.boolean,
+    vol.Optional(ATTR_NUMBER, default=0): cv.positive_int
+})
+
+SERVICE_PREVIOUS = vol.Schema({
+    vol.Required(ATTR_ENTITY_ID): cv.comp_entity_ids,
+    vol.Optional(ATTR_LIST_ID, default=""): cv.string,
+    vol.Optional(ATTR_URL, default=""): cv.string,
+    vol.Optional(ATTR_SONG_ID, default=""): cv.string,
+    vol.Optional(ATTR_NAME, default=""): cv.string,
+    vol.Optional(ATTR_REPEAT, default=False): cv.boolean,
+    vol.Optional(ATTR_NUMBER, default=0): cv.positive_int
+})
+def get_playlist_from_id_url(list_id, url):
+    id=''
+    if (list_id != ""):
+        id = list_id
+    else:
+        id = getListId(url)[0]
+    list_playlist = get_id_in_playlist(id)
+    return list_playlist
+
+def get_playlist_from_songid_name(song_id,url,name):
+    id = ""
+    if (song_id != ""):
+        id = song_id
+    elif (url != ""):
+        id = getVideoId(url)[0]
+    else: 
+        id = return_id_from_name(name)
+    song_url = return_url_from_id(id)  
+    return song_url,id 
+    
+class PlayerMedia:
+    def __init__(self, hass):
+        self.hass = hass
+        self.old_id_playlist = ''
+        self.old_playlist = []
+        self.id_song = ''
+        self.stop = False
+
+    def play_playlist(self, entity_id, list_playlist):
+        _LOGGER.info("list: %s", list_playlist)
+        for song_id in list_playlist:
+            if self.stop:
+                break
+            _LOGGER.info("Playing song with ID: %s", song_id)
+            self.id_song = song_id
+            yt = YouTube(return_url_from_id(song_id))
+            service_data = {
+                'entity_id': entity_id,
+                'media_content_id': return_url_from_id(song_id),
+                'media_content_type': 'music',
+                "extra": {
+                    "thumb": yt.thumbnail_url,
+                    "title": yt.title
+                }
+            }
+            self.hass.services.call('media_player', 'play_media', service_data)
+            sleep(3)
+            while True:
+                state = self.hass.states.get(entity_id)
+                if state.state == 'idle':
+                    break
+                sleep(1)
+
+    def play_next(self, entity_id, list_playlist):
+        current_index = list_playlist.index(self.id_song)
+        if current_index < len(list_playlist) - 1:
+            current_index += 1
+        self.stop = True
+        self.stop = False
+        self.play_playlist(entity_id, list_playlist[current_index:])
+
+    def play_previous(self, entity_id, list_playlist):
+        current_index = list_playlist.index(self.id_song)
+        if current_index > 0:
+            current_index -= 1
+        self.stop = True
+        self.stop = False
+        self.play_playlist(entity_id, list_playlist[current_index:])
+
+    def tts_handler(self, service):
         entity_id = service.data[ATTR_ENTITY_ID]
-        service_data = {}
-        if str(entity_id).find("mass") != -1:
-            clear_queue(entity_id)
-        service_play = {'entity_id': entity_id, 'command': 'play'}
-        if service.service == SERVICE_PLAY_LIST:
-            list_id = service.data.get(ATTR_LIST_ID)
-            _LOGGER.info("-"*20)
-            _LOGGER.info("list_id: %s", list_id)
-            _LOGGER.info("entity_id: %s", entity_id)
-            url = service.data.get(ATTR_URL)
-            id = ""
-            if (list_id != ""):
-                id = list_id
-            else:
-                id = getListId(url)[0]
-            list_playlist = get_id_in_playlist(id)
-            if str(entity_id).find("mass") == -1:
-                _LOGGER.info("mass ==-1")
-                service_data = {'entity_id': entity_id, 'media_content_id': return_url_from_id(list_playlist[0]), 'media_content_type': 'music', 'enqueue': 'replace'}
-                hass.services.call('media_player', 'play_media', service_data)
-                for ids in list_playlist[1:]:
-                    service_data = {'entity_id': entity_id, 'media_content_id': return_url_from_id(ids), 'media_content_type': 'music', 'enqueue': 'add'}
-                    hass.services.call('media_player', 'play_media', service_data)
-                # hass.services.call('mass', 'queue_command', service_play)
-            else:
-                _LOGGER.info("mass !=-1")
-                service_data = {'entity_id': entity_id, 'uri': return_url_from_id(list_playlist[0]), 'command': 'play_media', 'enqueue_mode': 'play'}
-                hass.services.call('mass', 'queue_command', service_data)
-                for ids in list_playlist[1:]:
-                    service_data = {'entity_id': entity_id, 'uri': return_url_from_id(ids), 'command': 'play_media', 'enqueue_mode': 'add'}
-                    hass.services.call('mass', 'queue_command', service_data)
-                hass.services.call('mass', 'queue_command', service_play)
-        elif service.service == SERVICE_PLAY_SONG:
-            song_id = service.data.get(ATTR_SONG_ID)
-            url = service.data.get(ATTR_URL)
-            id = ""
-            number = service.data.get(ATTR_NUMBER)
-            name = service.data.get(ATTR_NAME)
-            repeat = service.data.get(ATTR_REPEAT)
-            if (song_id != ""):
-                id = song_id
-            elif (url != ""):
-                id = getVideoId(url)[0]
-            else: 
-                id = return_id_from_name(name)
-            song_url = return_url_from_id(id)
-            if (repeat == True):
-                if str(entity_id).find("mass") == -1:
-                    service_data = {'entity_id': entity_id, 'media_content_id': song_url, 'media_content_type': 'music', 'enqueue': 'replace'}
-                    hass.services.call('media_player', 'play_media', service_data)
+        for entity in entity_id:
+            if service.service == SERVICE_PLAY_LIST:
+                list_id = service.data.get(ATTR_LIST_ID)
+                url = service.data.get(ATTR_URL)
+                list_playlist = get_playlist_from_id_url(list_id, url)
+                self.old_id_playlist = list_id
+                self.old_playlist = list_playlist
+                self.play_playlist(entity, list_playlist)
+            elif service.service == SERVICE_PLAY_SONG:
+                song_id = service.data.get(ATTR_SONG_ID)
+                url = service.data.get(ATTR_URL)
+                number = service.data.get(ATTR_NUMBER)
+                name = service.data.get(ATTR_NAME)
+                repeat = service.data.get(ATTR_REPEAT)
+                song_url, id = get_playlist_from_songid_name(song_id, url, name)
+                self.old_id_playlist = id
+                if repeat:
                     if number != 0:
-                        for i in range(0,number):
-                            service_data = {'entity_id': entity_id, 'media_content_id': song_url, 'media_content_type': 'music', 'enqueue': 'add'}
-                            hass.services.call('media_player', 'play_media', service_data)
-                    # hass.services.call('mass', 'queue_command', service_play)
+                        list_playlist = [song_url] * number
+                        self.old_playlist = list_playlist
+                        self.play_playlist(entity, list_playlist)
                 else:
-                    service_data = {'entity_id': entity_id, 'uri': song_url, 'command': 'play_media', 'enqueue_mode': 'play'}
-                    hass.services.call('mass', 'queue_command', service_data)
-                    for i in range(0,number):
-                        service_data = {'entity_id': entity_id, 'uri': song_url, 'command': 'play_media', 'enqueue_mode': 'add'}
-                        hass.services.call('mass', 'queue_command', service_data)
-                    hass.services.call('mass', 'queue_command', service_play)
-            else:
-                if str(entity_id).find("mass") == -1:
-                    # service data for 'CALL SERVICE' in Home Assistant
-                    service_data = {'entity_id': entity_id, 'media_content_id': song_url, 'media_content_type': 'music', 'enqueue': 'replace'}
-                    hass.services.call('media_player', 'play_media', service_data)
                     if number != 0:
-                        list_id_the_same = return_the_same_id(id, number)
-                        for ids in list_id_the_same:
-                            service_data = {'entity_id': entity_id, 'media_content_id': return_url_from_id(ids), 'media_content_type': 'music', 'enqueue': 'add'}
-                            hass.services.call('media_player', 'play_media', service_data)
-                    # hass.services.call('mass', 'queue_command', service_play)
-                else:
-                    list_id_the_same = return_the_same_id(id, number)
-                    # service data for 'CALL SERVICE' in Home Assistant
-                    service_data = {'entity_id': entity_id, 'uri': song_url, 'command': 'play_media', 'enqueue_mode': 'play'}
-                    hass.services.call('mass', 'queue_command', service_data)
-                    for ids in list_id_the_same:
-                        service_data = {'entity_id': entity_id, 'uri': return_url_from_id(ids), 'command': 'play_media', 'enqueue_mode': 'add'}
-                        hass.services.call('mass', 'queue_command', service_data)
-                    hass.services.call('mass', 'queue_command', service_play)
-    hass.services.register(DOMAIN, SERVICE_PLAY_SONG, tts_handler, schema=SERVICE_SONG)
-    hass.services.register(DOMAIN, SERVICE_PLAY_LIST, tts_handler, schema=SERVICE_LIST)
+                        list_id_the_same = self.return_the_same_id(id, number)
+                        self.old_playlist = list_id_the_same
+                        self.play_playlist(entity, list_id_the_same)
+            elif service.service == SERVICE_PLAY_NEXT:
+                self.handle_next_previous(service, entity, True)
+            elif service.service == SERVICE_PLAY_PREVIOUS:
+                self.handle_next_previous(service, entity, False)
+
+    def handle_next_previous(self, service, entity, is_next):
+        list_id = service.data.get(ATTR_LIST_ID)
+        song_id = service.data.get(ATTR_SONG_ID)
+        url = service.data.get(ATTR_URL)
+        number = service.data.get(ATTR_NUMBER)
+        name = service.data.get(ATTR_NAME)
+        repeat = service.data.get(ATTR_REPEAT)
+        id = ''
+        if name:
+            song_url, id = get_playlist_from_songid_name(song_id, url, name)
+        if list_id == self.old_id_playlist or id == self.old_id_playlist:
+            list_playlist = self.old_playlist
+            if is_next:
+                self.play_next(entity, list_playlist)
+            else:
+                self.play_previous(entity, list_playlist)
+
+    def register_services(self):
+        self.hass.services.register(DOMAIN, SERVICE_PLAY_SONG, self.tts_handler, schema=SERVICE_SONG)
+        self.hass.services.register(DOMAIN, SERVICE_PLAY_LIST, self.tts_handler, schema=SERVICE_LIST)
+        self.hass.services.register(DOMAIN, SERVICE_PLAY_NEXT, self.tts_handler, schema=SERVICE_NEXT)
+        self.hass.services.register(DOMAIN, SERVICE_PLAY_PREVIOUS, self.tts_handler, schema=SERVICE_PREVIOUS)    
+
+def setup(hass, config):
+    youtube_assistant = PlayerMedia(hass)
+    youtube_assistant.register_services()
     return True
